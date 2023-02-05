8e47d09d84dace3dd4ed92ca49bf8787e34302a0
robinmattern <robin.mattern@gmail.com>
2023-02-05 12:37:09 -0500

    p230203.05 Added alphabet selection links for Colaborators

--- server3/s36_mysql-data-api/server.js
+++ server3/s36_mysql-data-api/server.js
@@ -23,10 +23,11 @@ var pDB = mysql.createConnection( {
 		Welcome to IODD MySQL Express Server API.<br>
 		Use any of the following APIs:<br>
 		<div style="margin-left:20px">
-		  <a href="/members"                >/members</a> (not implemented yet)<br>
-		  <a href="/members-projects"       >/members-projects</a> (not implemented yet)<br>     
-		  <a href="/projects"               >/projects</a><br> 
-		  <a href="//project-colaborators"  >/project-colaborators</a><br> 
+		  <a href="/members"                      >/members</a> (not implemented yet)<br>
+		  <a href="/members-projects"             >/members-projects</a> (not implemented yet)<br>     
+		  <a href="/projects"                     >/projects</a><br> 
+		  <a href="/project-colaborators"         >/project-colaborators</a><br> 
+		  <a href="/project-colaborators-letters" >/project-colaborators-letters</a><br> 
 		</div>       
 		`);
 	} ) ;
@@ -34,16 +35,28 @@ var pDB = mysql.createConnection( {
 
 	app.get( '/projects', function( req, res ) {
 
-	var nRecs =  req.query.recs || 9999  
+   var  nRecs    =  req.query.recs    || 9999 
+   var  aLetters =  req.query.letters || ''
+
+    if (aLetters == "") {
    var 	aSQL  = `SELECT  * 
                    FROM  projects
                   WHERE  Id <= ${nRecs}`
+   } else {
+   var  aSQL  = `SELECT  * 
+                   FROM  members, members_projects, projects  
+                  WHERE  members.Id  = members_projects.MemberId
+                    AND  projects.Id = members_projects.ProjectId
+                    AND  LastName like '${aLetters}%' 
+               ORDER BY  Name, Lastname` 
+        }                  
 
 		pDB.query( aSQL, onQuery )
 	function onQuery( error, results, fields ) {
 		if ( error ) { 
 			console.log( `** Error: ${error.message}` );
-			res.send( `Error: ${error.message}` );
+//	        res.send( `Error: ${error.message}` );
+			res.send( JSON.stringify( { Projects: { Error: error.message } } ) ); // to conform to db.json structure 
 			res.end();
 			return
 		    }
@@ -52,7 +65,8 @@ var pDB = mysql.createConnection( {
 //			res.send( JSON.stringify(             results   ) );
 			res.send( JSON.stringify( { Projects: results } ) ); // to conform to db.json structure 
 		} else {
-			res.send( `{ error: "No projects found" }` );
+//       	res.send( `{ error: "No projects found" }` );
+			res.send( JSON.stringify( { Projects: { Error: "No projects found" } } ) ); // to conform to db.json structure 
 		}
 		res.end();
 		};
@@ -67,6 +81,7 @@ var pDB = mysql.createConnection( {
                 `WHERE  ProjectId = ${iId}` : `` }`
 
 		pDB.query( aSQL, onQuery )
+
 	function onQuery( error, results, fields ) {
 		if ( error ) { 
 			console.log( `** Error: ${error.message}` );
@@ -82,7 +97,42 @@ var pDB = mysql.createConnection( {
 		}
 		res.end();
 		};
-	} );
+	} );  // app.get( '/project-colaborators', ... ) 
+// -------------------------------------------------------------------------
+
+	app.get( '/project-colaborators-letters', function( req, res ) {
+
+   var  iId  =  req.query.id || 0  
+   var 	aSQL = `SELECT  Distinct substr(LastName,1,1) as Letter 
+                  FROM  members_projects_view
+               ORDER BY 1` 
+
+   var  aTable = "Letters"
+
+		pDB.query( aSQL, onQuery )  
+
+    function onQuery( error, results, fields ) {
+		if ( error ) { 
+			console.log( `** Error: ${error.message}` );
+			res.send( `Error: ${error.message}` );
+			res.end();
+			return
+		    }
+		if (results.length > 0) {
+			res.setHeader( 'Content-Type', 'application/json' );
+        var pJSON = { } 
+            pJSON[ aTable ] = results.map( pLetter => pLetter.Letter )    
+//          pJSON = { aTable : results }   // no workie 
+
+  			res.send( JSON.stringify( pJSON ) );
+//			res.send( JSON.stringify( { aTable      : results } ) );
+//			res.send( JSON.stringify( { Colaborators: results } ) );
+		} else {
+			res.send( `{ error: "No ${aTable} found!" }` );
+		}
+		res.end();
+        }
+	} );  // app.get( '/project-colaborators-letters', ... ) 
 // -------------------------------------------------------------------------
 
 	app.listen(3000);
